efed0ef9995ddef167b7ce7522976edf6e2d3960
Joao Paulo Furtado Silva <joaopaulofurtado@live.com>
2019-03-16 21:28:24 -0700

    hackachieve

--- js/config.js
+++ js/config.js
@@ -1,37 +1,37 @@
 const config = {
 
   // mode
-  'mode': 'prod', //please set to prod before pushing to github pages!
+  'mode': 'dev', //please set to prod before pushing to github pages!
   //prod mode will turn on tracking, so AVOID visiting your own page, since it will lower your conversion rate
   //I only set it to dev while adjusting variables here to avoid tracking my own page visits.
 
 
   //Tracking
 
-  'ideaname': 'picndine',
+  'ideaname': 'hackachieve',
 
 
   // Content
 
-  'headline': 'Discover new Recipes using AI',
-  'subtitle': 'Just by taking a picture of any food!',
-  'first-mobile-mockup':'images/picndine-main.png',
+  'headline': 'Double your personal productivity in half of time',
+  'subtitle': 'Use Scrum to leverage your life',
+  'first-mobile-mockup':'images/hackachieve-main.png',
 
 
 
 
   //Benefits
 
-  'second-mobile-mockup': 'images/picndine-pictures.png',
-  'second-mobile-title': 'Discover what to eat',
-  'second-mobile-description': 'Just take a picture of any food that you have in your home',
+  'second-mobile-mockup': 'images/hackachieve-initial-goals.png',
+  'second-mobile-title': 'Organize your life',
+  'second-mobile-description': 'Categorize your goals in health, finances, spiritual, or anything else that you would like',
 
 
-  'third-mobile-mockup': 'images/picndine-recipes.png',
-  'third-mobile-title': 'Get recipes based on recognized ingredients',
-  'third-mobile-description': 'Our AI system will recognize ingredients from your picture and suggest your recipes based on it.',
+  'third-mobile-mockup': 'images/hackachieve-track-goals.png',
+  'third-mobile-title': 'Track your long term and short term goals',
+  'third-mobile-description': 'Set deadlines and track progress, pin goals by priority and use a effort x impact matrix to maximize your efficiency.',
 
 
-  'footer-headline': 'Discover new Recipes using AI',
+  'footer-headline': 'Scrum for personal goals',
   'footer-subtitle': 'Get early access!'
 };
